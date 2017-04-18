d5c7149b11891328929d3cd6143ce54f45a01175
Rohaan Kewalramani <ronnie.kewal@gmail.com>
2017-04-18 17:02:44 -0400

    Make changes for heroku

--- frontend/lets_meet.jsx
+++ frontend/lets_meet.jsx
@@ -1,11 +1,12 @@
+//React
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { login, signup, logout } from './util/session_api_util';
+//Components
+import Root from './components/root';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const root = document.getElementById('root');
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
-  ReactDOM.render(<h2>Hello</h2>, root);
+  ReactDOM.render(<Root store={ store } />, root);
 });
