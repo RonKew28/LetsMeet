79c75566259bfbd9cc760ff414dbe08331cdb612
Rohaan Kewalramani <ronnie.kewal@gmail.com>
2017-04-18 12:40:29 -0400

    complete auth-part of User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,33 @@ class User < ApplicationRecord
 
   after_initialize: :ensure_session_token
 
-  def self.find_by_credentials()
-
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.is_password?(password) ? user : nil
   end
 
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(32)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+   BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+   self.session_token = User.generate_session_token
+   self.save!
+   self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
