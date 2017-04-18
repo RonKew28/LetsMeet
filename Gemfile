3eb21ff6bfeaf5879ac5cad4e802f139274040f7
Rohaan Kewalramani <ronnie.kewal@gmail.com>
2017-04-18 13:32:17 -0400

    Complete UsersController and SessionsController

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'coffee-rails', '~> 4.2'
 gem 'jquery-rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
+
+gem 'annotate'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,32 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+    user_params[:email],
+    user_params[:password]
+    )
+
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render(
+      json: { base: ["Invalid email or password"] },
+      status: 401
+      )
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      render "api/users/show"
+    else
+      render(
+      json: ["No one is signed in"],
+      status: 404
+      )
+    end
+  end
+
 end
