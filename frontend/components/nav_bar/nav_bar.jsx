30996bf8e82f47f7713f0538dbb9a9db78f90d3b
Rohaan Kewalramani <ronnie.kewal@gmail.com>
2017-04-19 18:42:25 -0400

    update nav bar component

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -2,34 +2,36 @@ import React from 'react';
 import { Link } from 'react-router';
 
 
-
-  const loggedOutLinks = () => (
-    <header className="main-nav">
-      <nav className="left-nav">
-        <ul>
-          <li><Link to='/login'>Log in</Link></li>
-          <li><Link to='/signup'>Sign up</Link></li>
-        </ul>
-      </nav>
-    </header>
-  );
+  const loggedOutLinks = login => (
+      <header className="main-nav">
+        <nav></nav>
+        <nav></nav>
+        <nav className="right-nav">
+          <ul>
+            <li><Link to='/login'>Log in</Link></li>
+            <li><Link to='/signup'>Sign up</Link></li>
+          </ul>
+        </nav>
+      </header>
+    );
 
   const loggedInLinks = (currentUser, logout) => (
-    <nav>
-      <div>
-        <button onClick={logout}>Log out</button>
-      </div>
-      <h3 className="red-button"><Link to='/'>View Profile</Link>{currentUser.email}</h3>
-    </nav>
+      <header className="main-nav">
+        <nav></nav>
+        <nav></nav>
+        <nav className="right-nav">
+          <ul>
+            <li><button onClick={logout}>Log out</button></li>
+          <li><h3 className="red-button"><Link to='/'>View Profile</Link>{currentUser.email}</h3></li>
+          </ul>
+        </nav>
+      </header>
     );
 
 
-  // display({ currentUser, logout }) {
-  //   return currentUser ? loggedInLinks(logout) : loggedOutLinks();
-  // }
+  const NavBar = ({ currentUser, logout, login }) => (
+      currentUser ? loggedInLinks(currentUser, logout) : loggedOutLinks(login)
+  );
 
-  const NavBar = ({ currentUser, logout }) => (
-    currentUser ? loggedInLinks(currentUser, logout) : loggedOutLinks()
-    );
 
 export default NavBar;
