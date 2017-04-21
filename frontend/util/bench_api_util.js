f1572509a626964a0bd607d577c6cc60cf9c118f
Rohaan Kewalramani <ronnie.kewal@gmail.com>
2017-04-21 11:39:57 -0400

    Create group api util functions

---
+++ frontend/util/bench_api_util.js
@@ -0,0 +1,35 @@
+export const fetchGroups = () => {
+  return $.ajax({
+    method: 'GET',
+    url: '/api/groups/',
+  });
+};
+
+export const fetchGroup = (id) => {
+  return $.ajax({
+    method: 'GET',
+    url: `/api/groups/${id}`,
+  });
+};
+
+export const createGroup = (group) => {
+  return $.ajax({
+    method: 'POST',
+    url: '/api/groups/',
+    data: { group }
+  });
+};
+
+export const updateGroup = (group) => {
+  return $.ajax({
+    method: 'PATCH',
+    url: `/api/groups/${group.id}`,
+  });
+};
+
+export const deleteGroup = (id) => {
+  return $.ajax({
+    method: 'DELETE',
+    url: `/api/groups/${id}`,
+  });
+};
