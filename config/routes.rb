3adaff7ccbc2be8567ff2bf8e7db5e60c8c96521
Rohaan Kewalramani <ronnie.kewal@gmail.com>
2017-04-21 10:33:58 -0400

    Edit Group model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy]
+    resources :groups, only: [:create, :destroy, :show, :index]
   end
   root "static_pages#root"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
+Group.destroy_all
 
-User.create!(email: "guest_user@guest.com", username: "guest", password: "password");
+User.create!(email: "guest_user@guest.com", username: "guest", password: "password")
+
+Group.create!(name: "Tennis", description: "Tennis", creator_id: 1, location: "New York", founded_date: Date.today)
