6d8a79511ad05f2d0538c06129f9fcf7f48c502f
Rohaan Kewalramani <ronnie@Rohaans-MacBook-Air.local>
2017-04-22 23:44:17 -0400

    Create membership controller and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy]
     resources :groups, only: [:create, :update, :destroy, :show, :index]
+    resources :memberships, only: [:create, :destroy]
   end
   root "static_pages#root"
 end
