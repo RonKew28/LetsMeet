7303d7d4a0efd4c4fb642fd045972ba226bb59a3
Rohaan Kewalramani <ronnie.kewal@gmail.com>
2017-04-23 01:37:59 -0400

    Work on group navigation bar

---
+++ frontend/components/groups/group_show.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router';
+import GroupNavBar from './group_nav_bar';
+
+
+class GroupsShow extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <GroupNavBar />;
+  }
+}
