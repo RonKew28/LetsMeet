77a0ee5a13cbbfa32439b8bd5474951f46ab6ff5
Rohaan Kewalramani <ronnie.kewal@gmail.com>
2017-04-23 13:39:34 -0400

    Fix bugs in group show page

--- frontend/components/groups/group_show.jsx
+++ frontend/components/groups/group_show.jsx
@@ -3,12 +3,22 @@ import { Link, withRouter } from 'react-router';
 import GroupNavBar from './group_nav_bar';
 
 
-class GroupsShow extends React.Component {
+class GroupShow extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {
+    this.props.fetchGroup(this.props.groupId);
+  }
+
+
   render() {
-    <GroupNavBar {this.props} />;
+    debugger
+    return(
+    <h1>{this.props.group.name}</h1>
+  );
   }
 }
+
+export default GroupShow;
