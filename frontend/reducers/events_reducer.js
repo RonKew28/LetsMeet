a258b05c1a9f148c4c585903387d90490877b547
Rohaan Kewalramani <ronnie.kewal@gmail.com>
2017-04-24 13:05:46 -0400

    Create EventsReducer, update RootReducer, add event selector methods to selectors file

---
+++ frontend/reducers/events_reducer.js
@@ -0,0 +1,29 @@
+import {
+  RECEIVE_EVENTS,
+  RECEIVE_EVENT,
+  REMOVE_EVENT, RECEIVE_ERRORS } from '../actions/event_actions';
+import { merge } from 'lodash';
+
+
+const _nullEvent = {};
+
+const EventsReducer = (state = _nullEvent, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_EVENTS:
+      return action.events;
+    case RECEIVE_EVENT:
+      return merge({}, state, { [action.event.id]: action.event });
+    case REMOVE_EVENT:
+      let newState = merge({}, state);
+      delete newState[action.event.id];
+      return newState;
+    case RECEIVE_ERRORS:
+      const errors = action.errors;
+      return merge({}, state, { errors: action.errors });
+    default:
+      return state;
+  }
+};
+
+export default EventsReducer;
