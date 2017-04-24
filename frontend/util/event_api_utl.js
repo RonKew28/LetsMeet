ea46be5c8e5bbd0173b9ab2690428bb23dea213f
Rohaan Kewalramani <ronnie.kewal@gmail.com>
2017-04-24 12:55:50 -0400

    Create event util actions

---
+++ frontend/util/event_api_utl.js
@@ -0,0 +1,36 @@
+export const fetchEvents = () => {
+  return $.ajax({
+    method: 'GET',
+    url: '/api/events',
+  });
+};
+
+export const fetchEvent = (id) => {
+  return $.ajax({
+    method: 'GET',
+    url: `/api/events/${id}`,
+  });
+};
+
+export const createEvent = (event) => {
+  return $.ajax({
+    method: 'POST',
+    url: '/api/events',
+    data: { event }
+  });
+};
+
+export const updateEvent = (event) => {
+  return $.ajax({
+    method: 'PATCH',
+    url: `/api/events/${event.id}`,
+    data: { event }
+  });
+};
+
+export const deleteEvent = (id) => {
+  return $.ajax({
+    method: 'DELETE',
+    url: `/api/events/${id}`,
+  });
+};
