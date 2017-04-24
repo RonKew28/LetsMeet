0ea7e70562663a948d6f1f464aeb1eda60fb3162
Rohaan Kewalramani <ronnie.kewal@gmail.com>
2017-04-24 12:51:11 -0400

    Create events controller

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,35 @@
+class Api::EventsController < ApplicationController
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      render :show
+    else
+      render json: @event.errors.messages, status: 422
+    end
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update
+      render :show
+    else
+      render json: @event.errors.messages, status: 422
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @group = Group.find(@event.group.id)
+    if @event.destroy
+      render 'api/groups/show'
+    else
+      render json: @event.errors.messages, status: 422
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :description, :date, :group_id, :organizer_id, :location)
+  end
+end
